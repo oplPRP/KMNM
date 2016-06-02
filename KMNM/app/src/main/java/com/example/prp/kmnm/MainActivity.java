c1dfcc4b6a53ed044e5da83d9dcb2b8f45df2a42
eitaro_mori <a61-201405-2055@61-201405-2055-no-Mac-mini.local>
2016-06-02 16:25:21 +0900

    ホーム画面の作成 ＊要画像の差し替え

--- KMNM/app/src/main/java/com/example/prp/kmnm/MainActivity.java
+++ KMNM/app/src/main/java/com/example/prp/kmnm/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.prp.kmnm;
 
+import android.app.Activity;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
