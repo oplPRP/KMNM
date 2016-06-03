e231571e7135a1c158a8cc4eb837225a668e6745
eitaro_mori <a61-201405-2055@61-201405-2055-no-Mac-mini.local>
2016-06-03 18:35:24 +0900

    乱数を作り、タップ数が設定された乱数と同じになった時に画像が表示される 画像表示後遷移予定（未実装）

--- KMNM/app/src/main/java/com/example/prp/kmnm/GameActivity.java
+++ KMNM/app/src/main/java/com/example/prp/kmnm/GameActivity.java
@@ -1,27 +1,59 @@
 package com.example.prp.kmnm;
 
+import android.app.Fragment;
+import android.graphics.Camera;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.app.Activity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 /**
  * Created by a61-201405-2055 on 16/06/03.
  */
 public class GameActivity extends Activity implements View.OnClickListener{
 
+    int clickCounter;
+    int randomNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTra
+//        }
+
         // 画像付きボタン
         ImageButton gameBtn = (ImageButton)findViewById(R.id.gamePushBtn);
         gameBtn.setOnClickListener(this);
+
+        //乱数生成
+        randomNumber = (int)(Math.random()*30)+1;
     }
 
     public void onClick (View view) {
-
+        clickCounter = clickCounter + 1;
+        Log.d("乱数", String.valueOf(randomNumber));
+        Log.d("クリック数", String.valueOf(clickCounter));
+        //乱数とタップ数が同じになった時に画像を表示
+        if (randomNumber == clickCounter){
+            ImageView hitImage = (ImageView)findViewById(R.id.game_hit);
+            hitImage.setImageResource(R.drawable.game_hit);
+            final Handler timer = new Handler();
+            timer.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    findViewById(R.id.game_hit).setVisibility(View.INVISIBLE);
+                }
+            }, 1000);
+        }
     }
 }
